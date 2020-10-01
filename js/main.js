4a24d6a0c67b5315ccf0f8cb51d3edbe430c48a1
Karina-N <nektarinas@gmail.com>
2020-10-01 10:16:02 +0300

    added feed

---
+++ js/main.js
@@ -0,0 +1,9 @@
+import posts from './data.js';
+import renderPosts from './renderPosts.js';
+
+<<<<<<< HEAD
+renderPosts(posts);
+=======
+renderPosts(posts)
+
+>>>>>>> ed23cf842289bfdb6b7bf8506e88e28a177c5d3c
